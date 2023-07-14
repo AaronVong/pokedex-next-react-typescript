c8276dce9eeffab5b75c338177c8982b9c02d160
Minh Vong <minhvong66923@gmail.com>
2023-07-14 15:57:20 +0700

    header and footer components

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+/**
+ * @file This file contains Footer component
+ */
+
+import { Box, Grid } from "@mui/material";
+
+export default function Footer() {
+  return <Box component={"footer"}>This is the footer</Box>;
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+/**
+ * @file This file contains Header component
+ */
+
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }} component={"header"}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
